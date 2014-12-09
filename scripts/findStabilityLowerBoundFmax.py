f38edd9d33ae522771dcd4ff140d8aa192928aef
sjkdenny <sjkdenny@gmail.com>
2014-12-09 12:50:41 -0800

    makes plots for range of lower bound fmax

--- scripts/findStabilityLowerBoundFmax.py
+++ scripts/findStabilityLowerBoundFmax.py
@@ -17,6 +17,8 @@ import scipy.io as sio
 import CPlibs
 import IMlibs
 import fittingParameters
+from matplotlib import rc
+rc('text', usetex=True)
 
 # may want to make sure path is right here
 parameters = fittingParameters.Parameters()
@@ -46,8 +48,10 @@ fitParametersFilenameParts = IMlibs.getfitParametersFilenameParts(bindingSeriesF
 
 # fit with different fmaxes
 fmax_lbs = np.arange(0, 2, 0.1)
-resulting_dGs = np.zeros((len(fmax_lbs), len(indx_subset)))
-resulting_var_dGs = np.zeros((len(fmax_lbs), len(indx_subset)))
+num_clusters = len(indx_subset)
+num_lbs = len(fmax_lbs)
+resulting_dGs = np.zeros((num_lbs, num_clusters))
+resulting_var_dGs = np.zeros((num_lbs, num_clusters))
 
 for j, fmax_lb in enumerate(fmax_lbs):
     workerPool = multiprocessing.Pool(processes=numCores) #create a multiprocessing pool that uses at most the specified number of cores
@@ -69,3 +73,49 @@ for j, fmax_lb in enumerate(fmax_lbs):
     resulting_dGs[j] = fitParameters['dG']
     resulting_var_dGs[j] = fitParameters['dG_var']
     resulting_qvalues = fitParameters['qvalue']
+    
+# plot with error bars
+dirname = 'binding_curves_rigid'
+subset = np.arange(num_clusters)[np.array(resulting_qvalues < 0.05)]
+fig = plt.figure(figsize=(5,4))
+ax = fig.add_subplot(111)
+for i in subset[:200]:
+    ax.errorbar(fmax_lbs, resulting_dGs[:,i]-resulting_dGs[0,i], yerr=resulting_var_dGs[:,i], fmt='-o', color = 'r', ecolor='k', alpha = 0.1)
+ax.set_ylim((-2, 7))
+ax.set_xlabel('lower bound fmax')
+ax.set_ylabel('ddG')
+plt.tight_layout()
+plt.savefig('%s/lower_bound_fmax_stability.below_qvalue.png'%(dirname))
+
+subset = np.arange(num_clusters)[np.array(resulting_qvalues > 0.05)]
+fig = plt.figure(figsize=(5,4))
+ax = fig.add_subplot(111)
+for i in subset[:200]:
+    ax.errorbar(fmax_lbs, resulting_dGs[:,i]-resulting_dGs[0,i], yerr=resulting_var_dGs[:,i], fmt='-o', color = 'b', ecolor='k', alpha = 0.1)
+ax.set_ylim((-2, 7))
+ax.set_xlabel('lower bound fmax')
+ax.set_ylabel('ddG')
+plt.tight_layout()
+plt.savefig('%s/lower_bound_fmax_stability.above_qvalue.png'%(dirname))
+
+# get fraction that change from previous point
+threshold = 0.1 # kcal/mol
+matrix = np.zeros((num_lbs, num_clusters), dtype=bool)
+for i in range(1, num_lbs):
+    # for each lower bound, is it different from previous?
+    matrix[i] = np.abs(resulting_dGs[i] - resulting_dGs[0]) > resulting_var_dGs[i]
+# plot
+fig = plt.figure(figsize=(5,4))
+ax = fig.add_subplot(111)
+# first do those below qvalue
+subset_below = np.arange(num_clusters)[np.array(resulting_qvalues < 0.05)]
+subset_above = np.arange(num_clusters)[np.array(resulting_qvalues > 0.05)]
+ax.plot(fmax_lbs, np.sum(matrix[:, subset_below], axis=1)/float(len(subset_below)), 'r', label='below FDR')
+ax.plot(fmax_lbs, np.sum(matrix[:, subset_above], axis=1)/float(len(subset_above)), 'b', label='above FDR')
+ax.set_xlabel('lower bound fmax')
+ax.set_ylabel('fraction that significantly differ from lb=0')
+ax.set_ylim((0, 1))
+handles,labels = ax.get_legend_handles_labels()
+ax.legend(handles, labels, loc='upper right')
+plt.tight_layout()
+plt.savefig('%s/lower_bound_fmax_stability.fraction_below_var_from0.pdf'%(dirname))
