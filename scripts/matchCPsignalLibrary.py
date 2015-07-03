d5b0fac2acb82a09f5417a428528fefe1d4db448
sjkdenny <sjkdenny@gmail.com>
2015-07-02 22:03:37 -0700

    changed 'mapping' to just using barcodes as indexes rather than searching

--- scripts/matchCPsignalLibrary.py
+++ scripts/matchCPsignalLibrary.py
@@ -41,11 +41,11 @@ def findSeqMapDict(cpsignal,barcodeMap, is_designed):
     names = [col for col in cpsignal] + [col for col in barcodeMap]
     seqMap = pd.DataFrame(data=np.nan, columns=names, index=np.arange(len(cpsignal))) 
     for name in  [col for col in cpsignal]:
-        seqMap[name] = np.array(cpsignal[name])
+        seqMap.loc[:,name] = np.array(cpsignal[name])
     indices_cpsignal = np.arange(len(cpsignal))[is_designed > -1]
     indices_barcode = is_designed[is_designed > -1]
     for name in [col for col in barcodeMap]:
-        seqMap[name].iloc[indices_cpsignal] = np.array(barcodeMap[name].iloc[indices_barcode])
+        seqMap.loc[:,name].iloc[indices_cpsignal] = np.array(barcodeMap[name].iloc[indices_barcode])
     return seqMap
     
 
@@ -58,16 +58,27 @@ if __name__ == '__main__':
 
     print "loading CP signal file ..."
     cpsignal = IMlibs.loadCPseqSignal(cpSignalFilename)
-    cpsignal.sort('index1_seq', inplace=True)
+    # if there is no barcode, add the first 16 bases, just in case that matches one of the mapped ones
+    index = cpsignal.index1_seq.isnull()
+    cpsignal.loc[index, 'index1_seq'] = [s[:16] for s in cpsignal.loc[index, 'read1_seq']]
+    cpsignal.index = cpsignal.index1_seq
     
     print "loading barcode Map..."
     barcodeMap = pd.read_table(barcodeMapFilename)
+    barcodeMap.index = barcodeMap.barcode
+    
+    print "making final barcode to sequence mapping file"
+    seqMap = pd.concat([cpsignal, barcodeMap.loc[cpsignal.index1_seq]], axis=1)
+    seqMap.sort('variant_number', inplace=True)
+    IMlibs.saveDataFrame(seqMap, outFile, index=False, float_format='%4.0f')
+    
+    sys.exit()
     num_bc_per_variant, is_designed = IMlibs.findSequenceRepresentation(np.array(cpsignal['index1_seq']),
                                                                         np.array(barcodeMap['barcode']),
                                                                         exact_match = True)
     # get dict of barcode map
-    print "making final barcode to sequence mapping file"
+   
     seqMap = findSeqMapDict(cpsignal,barcodeMap, is_designed)
-    seqMap.sort('variant_number', inplace=True)
+    
     IMlibs.saveDataFrame(seqMap, outFile, index=False, float_format='%4.0f')
 
