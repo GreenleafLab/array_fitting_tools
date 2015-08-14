0233da25ab9faf44cb1b8f82072dc6f28573e80d
sjkdenny <sjkdenny@gmail.com>
2015-08-13 23:19:19 -0700

    more plots

--- bin/plotFun.py
+++ bin/plotFun.py
@@ -221,6 +221,86 @@ def plotNumberInBins(variant_table, xdelta=None):
                  fontsize=12)
     return
 
+def plotNumberTotal(variant_table, binedges=None, variant_table2=None):
+    bins = np.arange(100)
+    fig = plt.figure(figsize=(2.5,2.5))
+    hist, binedges, patches = plt.hist(variant_table.numTests.astype(float).values,
+                            bins=bins, histtype='stepfilled', alpha=0.5, color='grey')
+    plt.plot((binedges[:-1]+binedges[1:])*0.5, hist, linewidth=1, alpha=0.1, color='grey' )
+    #sns.distplot(variant_table.numTests.astype(float).values, bins=bins,
+    #            hist_kws={'histtype':'stepfilled'}, color='grey')
+    plt.xlabel('# measurements')
+    plt.ylabel('# variants')
+    plt.tight_layout()
+    ax = plt.gca()
+    ax.tick_params(top='off', right='off')
+
+    if variant_table2 is not None:
+        hist, binedges, patches = plt.hist(variant_table2.numTests.astype(float).values,
+                                           bins=bins, histtype='stepfilled', alpha=0.5, color=sns.xkcd_rgb['gold'])
+        plt.plot((binedges[:-1]+binedges[1:])*0.5, hist, linewidth=1, alpha=0.1, color=sns.xkcd_rgb['gold'])
+
+    ylim = ax.get_ylim()
+    plt.plot([5]*2, ylim, 'k--', linewidth=1, alpha=0.5)
+
+def findKdAndError(variant_table):
+    parameters = fitFun.fittingParameters()
+    kds = parameters.find_Kd_from_dG(variant_table.dG.astype(float))
+    error = [(fitFun.errorProgagationKdFromdG(variant_table.dG,
+                                             variant_table.dG - variant_table.dG_lb)/
+              kds),
+             (fitFun.errorProgagationKdFromdG(variant_table.dG,
+                                              variant_table.dG_ub - variant_table.dG)/
+              kds)]
+    fractional_error = pd.concat([variant_table.numTests,
+                                  pd.concat(error, axis=1).mean(axis=1)],
+        axis=1, keys=['numTests', 'fractional_error'])
+    return fractional_error
+
+def plotErrorTotal(variant_table, variant_table2=None):
+    
+    parameters = fitFun.fittingParameters()
+    
+    fig = plt.figure(figsize=(4,2.5))
+    ax = fig.add_subplot(111)
+    order = np.arange(100)
+
+    if variant_table2 is not None:
+        index = variant_table2.dG < parameters.find_dG_from_Kd(5000)
+        fractional_error = findKdAndError(variant_table2)
+        sns.boxplot(x="numTests", y="fractional_error", data=fractional_error.loc[index],
+                    whis=1.5, order=order, fliersize=0,
+                    linewidth=0.5, color=sns.xkcd_rgb['gold'], ax=ax, showcaps=False,
+                    showfliers=False)
+
+    index = variant_table.dG < parameters.find_dG_from_Kd(5000)
+    fractional_error = findKdAndError(variant_table)
+    sns.boxplot(x="numTests", y="fractional_error", data=fractional_error.loc[index],
+                whis=1.5, order=order, fliersize=0,
+                linewidth=0.5, color='grey', ax=ax, showcaps=False, showfliers=False)
+    
+    if len(order) < 100:
+        binwidth = 5
+    elif len(order) < 200:
+        binwidth = 10
+    elif len(order) < 300:
+        binwidth = 15
+    elif len(order) < 400:
+        binwidth = 20
+    ax = plt.gca()
+    ax.tick_params(top='off', right='off')
+
+    xticks = ax.get_xticks()
+    ax.set_xticks(xticks[::binwidth])
+    ax.set_xticklabels(['%d'%x for x in order[::binwidth]])
+
+    plt.xlim(0, 60)
+    plt.xlabel('# measurements')
+    plt.ylabel('fractional error on $K_d$')
+    plt.tight_layout()
+    ylim = ax.get_ylim()
+    plt.plot([5]*2, ylim, 'k--', linewidth=1, alpha=0.5)
+
 def plotFractionFit(variant_table, binedges=None):
     # plot
     binwidth=0.01
@@ -376,4 +456,172 @@ def plotDeltaAbsFluorescence(bindingSeries, bindingSeriesBackground, concentrati
                  horizontalalignment='left', verticalalignment='center',
                  fontsize=12)
     pass
+
+def plotReplicatesKd(variant_tables, cutoff=None, relative=None, variant=None,
+                   log=None, vmax=None):
+    if cutoff is None:
+        cutoff = -7.1
+    if relative is None:
+        relative = False
+    if variant is None:
+        variant = 34429
+    if log is None:
+        log = False
+    if log:
+        bins = 'log'
+    else:
+        bins = None
     
+    parameters = fitFun.fittingParameters()
+    
+    index = (pd.concat(variant_tables, axis=1).loc[:, 'numTests'] >=5).all(axis=1)
+    
+    x = np.log10(parameters.find_Kd_from_dG(variant_tables[0].loc[index].dG.astype(float)))
+    y = np.log10(parameters.find_Kd_from_dG(variant_tables[1].loc[index].dG.astype(float)))
+        
+    fig = plt.figure(figsize=(4,2.5))
+    ax = fig.add_subplot(111, aspect='equal')
+    im = ax.hexbin(x, y, bins=bins, mincnt=0, vmin=0, vmax=vmax,
+           #extent=[-12, cutoff, -12, cutoff],
+           gridsize=75)
+    
+    plt.xlabel('$K_d$ rep 1 (nM)')
+    plt.ylabel('$K_d$ rep 2 (nM)')
+    ax.tick_params(right='off', top='off')
+    
+    xlim = np.array(ax.get_xlim())
+    ylim = np.array(ax.get_ylim())
+    
+    index = (pd.concat(variant_tables, axis=1).loc[index, 'dG'] < cutoff).all(axis=1)
+    slope, intercept, r_value, p_value, std_err = st.linregress(x.loc[index],y.loc[index])
+    plt.plot(xlim, slope*xlim+intercept, 'r:', linewidth=1)
+
+    plt.annotate('$R^2$=%4.2f'%(r_value**2),
+                 xy=(0.95, 0.05),
+                 xycoords='axes fraction',
+                 horizontalalignment='right', verticalalignment='bottom',
+                 fontsize=12)
+    plt.xlim(xlim[0], x.max())
+    plt.ylim(ylim[0], y.max())
+    xticks = ax.get_xticks()
+    plt.xticks(xticks, ['$10^{%d}$'%x for x in xticks])
+    plt.yticks(xticks, ['$10^{%d}$'%x for x in xticks])
+    
+    plt.plot([np.log10(parameters.find_Kd_from_dG(cutoff))]*2, ylim, 'k--')
+    plt.plot(xlim, [np.log10(parameters.find_Kd_from_dG(cutoff))]*2, 'k--')
+    plt.colorbar(im)
+    #plt.yticks(np.arange(0, 60, 10))
+    plt.tight_layout()
+
+def plotReplicates(variant_tables, cutoff=None, relative=None, variant=None,
+                   log=None, vmax=None):
+    if cutoff is None:
+        cutoff = -7
+    if relative is None:
+        relative = False
+    if variant is None:
+        variant = 34429
+    if log is None:
+        log = False
+    if log:
+        bins = 'log'
+    else:
+        bins = None
+    
+    index = ((pd.concat(variant_tables, axis=1).loc[:, 'dG'] < -7).all(axis=1)&
+        ((pd.concat(variant_tables, axis=1).loc[:, 'numTests']) >=5).all(axis=1))
+    
+    x = variant_tables[0].loc[index].dG
+    y = variant_tables[1].loc[index].dG
+    
+    if relative:
+        x = x - x.loc[variant]
+        y = y - y.loc[variant]
+        text = '$\Delta\Delta$'
+        
+    else:
+        text = '$\Delta$'
+    
+    fig = plt.figure(figsize=(4,2.5))
+    ax = fig.add_subplot(111, aspect='equal')
+    im = ax.hexbin(x, y, bins=bins, mincnt=0, vmin=0, vmax=vmax,
+           #extent=[-12, cutoff, -12, cutoff],
+           gridsize=75)
+    
+    plt.xlabel('%s$G$ rep 1 (kcal/mol)'%text)
+    plt.ylabel('%s$G$ rep 2 (kcal/mol)'%text)
+    ax.tick_params(right='off', top='off')
+    
+    xlim = np.array(ax.get_xlim())
+    ylim = np.array(ax.get_ylim())
+    
+    slope, intercept, r_value, p_value, std_err = st.linregress(x,y)
+    plt.plot(xlim, slope*xlim+intercept, 'r:', linewidth=1)
+
+    plt.annotate('$R^2$=%4.2f'%(r_value**2),
+                 xy=(0.95, 0.05),
+                 xycoords='axes fraction',
+                 horizontalalignment='right', verticalalignment='bottom',
+                 fontsize=12)
+    plt.xlim(xlim[0], x.max())
+    plt.ylim(ylim[0], y.max())
+    plt.colorbar(im)
+    #plt.yticks(np.arange(0, 60, 10))
+    plt.tight_layout()
+
+def plotColoredByLength(variant_tables, cutoff=None):
+    if cutoff is None:
+        cutoff = -7
+    index = ((pd.concat(variant_tables, axis=1).loc[:, 'dG'] < cutoff).all(axis=1)&
+        ((pd.concat(variant_tables, axis=1).loc[:, 'numTests']) >=5).all(axis=1))
+
+    colors =  ["#e74c3c", "#3498db", "#34495e", "#9b59b6","#2ecc71"]
+    fig = plt.figure(figsize=(4, 2.5))
+    ax = fig.add_subplot(111)
+    for i, length in enumerate([10, 9, 11]):
+        index2 = index&(variant_tables[0].length == length)
+        plt.scatter(variant_tables[0].loc[index2].dG, variant_tables[1].loc[index2].dG,
+                marker='.', alpha=0.5, facecolors=colors[i], edgecolors='none', label='%dbp'%length)
+    plt.legend()
+    
+    plt.xlabel('$\Delta$$G$ rep 1 (kcal/mol)')
+    plt.ylabel('$\Delta$$G$ rep 2 (kcal/mol)')
+    ax.tick_params(right='off', top='off')
+    ax.set_position([0.2, 0.25, 0.5, 0.75])
+    
+    # Put a legend to the right of the current axis
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+    xlim = np.array(ax.get_xlim())
+    ylim = np.array(ax.get_ylim())
+    plt.xlim(xlim[0], cutoff)
+    plt.ylim(ylim[0], cutoff)
+    #plt.tight_layout()
+    
+def plotDeltaDeltaGByLength(variant_tables, cutoff=None, normed=None):
+    if cutoff is None:
+        cutoff = -7
+    if normed is None:
+        normed = False
+    index = ((pd.concat(variant_tables, axis=1).loc[:, 'dG'] < cutoff).all(axis=1)&
+        ((pd.concat(variant_tables, axis=1).loc[:, 'numTests']) >=5).all(axis=1))
+    ddG = variant_tables[1].dG - variant_tables[0].dG
+    
+    bins = np.linspace(ddG.loc[index].min(),ddG.loc[index].max() )
+    colors =  ["#e74c3c", "#3498db", "#34495e", "#9b59b6","#2ecc71"]
+    fig = plt.figure(figsize=(4, 2.5))
+    ax = fig.add_subplot(111)
+    for i, length in enumerate([10, 9, 11]):
+        index2 = index&(variant_tables[0].length == length)
+        if normed:
+            sns.distplot(ddG.loc[index2], hist_kws={'histtype':'stepfilled'},
+                     label='%dbp'%length, color=colors[i])
+        else:
+            plt.hist(ddG.loc[index2].values, bins=bins, histtype='stepfilled',
+                     label='%dbp'%length, color=colors[i], alpha=0.5)
+    plt.legend(loc='upper left')
+    ax.tick_params(right='off', top='off')
+    plt.xlabel('$\Delta\Delta$G (kcal/mol)')
+    plt.ylabel('probability')
+    plt.tight_layout()
+
