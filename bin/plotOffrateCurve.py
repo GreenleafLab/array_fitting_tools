8864bac027100f9158d1d2533e039a67b7cf195d
sjkdenny <sjkdenny@gmail.com>
2016-05-30 16:13:12 -0700

    changed a bunch. no longer takes binned arguments, will only plot a single tile.

--- bin/plotOffrateCurve.py
+++ bin/plotOffrateCurve.py
@@ -29,6 +29,7 @@ import plotFun
 import findFmaxDist
 import fitFun
 import fileFun
+import variantFun
 #plt.rc('text', usetex=True)
 ### MAIN ###
 
@@ -38,53 +39,61 @@ import fileFun
 parser = argparse.ArgumentParser(description='bootstrap fits')
 parser.add_argument('-f', '--variant_file', required=True, metavar=".CPvariant",
                    help='file with bootstrapped, variant fits')
-parser.add_argument('-ts', '--time_series', required=True, metavar=".CPtimeseries.pkl",
-                   help='file containining the fluorescence information'
-                   ' binned over time.')
-parser.add_argument('-a', '--annotated_clusters', required=True, metavar=".CPannot.pkl",
-                   help='file with clusters annotated by variant number')
-parser.add_argument('-t', '--times', metavar=".times",
-                   help='file containining the binned times')
-parser.add_argument('-l', '--lib_characterization', 
-                   help='library characterization filename. Use if supplying variant'
-                   'sequence of variant index name')
+parser.add_argument('-cs', '--cpseries', metavar="CPseries.pkl",
+                   help='CPseries file containining the time series information')
+parser.add_argument('-td', '--time_dict', metavar="timeDict.p",
+                   help='file containining the timing information per tile')
+parser.add_argument('--tile', metavar="NNN", default='001',
+                   help='plot one this tiles median values')
+
 parser.add_argument('-out', '--out_file', 
                    help='output filename. default is variant number + ".pdf"')
 parser.add_argument('--annotate', action="store_true",
                    help='flag if you want the plot annotated')
 
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-v', '--variant_numbers', nargs='+', metavar="N", type=int,
+group = parser.add_argument_group()
+group.add_argument('-v', '--variant_numbers', nargs='+', metavar="N", 
                    help='index of variant(s) to plot')
+group.add_argument('-an', '--annotated_clusters', metavar="CPannot.pkl",
+                   help='annotated cluster file. Supply if you wish to take medians per variant.'
+                   'Otherwise assumes single cluster fits.')
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
     
     variantFilename = args.variant_file
-    annotatedClusterFile  = args.annotated_clusters
-    bindingCurveFilename  = args.time_series
-
-
+    bindingSeriesFile = args.cpseries
+    timeDeltaFile = args.time_dict
+    annotatedClusterFile = args.annotated_clusters
+    tile_to_subset = args.tile
     
     # find variant
-    variant_table = pd.read_table(variantFilename, index_col=0)
-    if not np.any(variant_table.loc[args.variant_numbers].numTests > 0):
-        print 'Error: no clusters on chip with any of these variant numbers!'
-        sys.exit()
+    variant_table = fileFun.loadFile(variantFilename)
+    #if not np.any(variant_table.loc[args.variant_numbers].numTests > 0):
+    #    print 'Error: no clusters on chip with any of these variant numbers!'
+    #    sys.exit()
 
     # load data
-    times = fileFun.loadFile(args.times)
-    annotatedClusters = pd.read_pickle(annotatedClusterFile)
-    bindingSeries = pd.read_pickle(bindingCurveFilename)
-    groupedBindingSeries = pd.concat([annotatedClusters, bindingSeries], axis=1).groupby('variant_number')
+    bindingSeries = fileFun.loadFile(bindingSeriesFile)
+    timeDict = fileFun.loadFile(timeDeltaFile)
 
-    offRates = variantFun.perVariant(variant_table, groupedBindingSeries, times)  
+    if annotatedClusterFile is not None:
+        annotatedClusters = fileFun.loadFile(annotatedClusterFile)
+    else:
+        annotatedClusters = pd.DataFrame(bindingSeries.index.tolist(), index=bindingSeries.index, columns=['variant_number'])
+
+    offRates = variantFun.perVariant(variant_table, annotatedClusters, bindingSeries, x=timeDict[tile_to_subset])
     
-    for variant in args.variant_numbers:  
-        offRates.plotOffrateCurve(variant)
+    for variant in args.variant_numbers:
+        try:
+            variant = int(variant)
+        except ValueError:
+            pass
+        
+        offRates.plotOffrateCurve(variant, annotate=args.annotate)
         
         if args.out_file is None:
-            args.out_file = 'binding_curve.variant_%d.pdf'%variant
+            args.out_file = 'binding_curve.variant_%s.pdf'%str(variant)
         
-    plt.savefig( args.out_file)
+        plt.savefig( args.out_file)
