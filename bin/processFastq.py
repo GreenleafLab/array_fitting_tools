a66957af3e773d253e7e0e60c5b891d9f2ee7d39
sjkdenny <sjkdenny@gmail.com>
2018-03-22 16:26:53 -0700

    removed batch iterator func

--- bin/processFastq.py
+++ bin/processFastq.py
@@ -80,32 +80,7 @@ def getScorePvalue(nwScore, m, n, k=0.0097, l=0.5735, nwScoreScale=0.2773):
     score = nwScore*nwScoreScale
     return (1 - np.exp(-np.exp(-l*(score-u))))
 
-def batch_iterator(iterator, batch_size):
-    """Returns lists of length batch_size.
 
-    This can be used on any iterator, for example to batch up
-    SeqRecord objects from Bio.SeqIO.parse(...), or to batch
-    Alignment objects from Bio.AlignIO.parse(...), or simply
-    lines from a file handle.
-
-    This is a generator function, and it returns lists of the
-    entries from the supplied iterator.  Each list will have
-    batch_size entries, although the final list may be shorter.
-    """
-    entry = True  # Make sure we loop once
-    while entry:
-        batch = []
-        while len(batch) < batch_size:
-            try:
-                entry = iterator.next()
-            except StopIteration:
-                entry = None
-            if entry is None:
-                # End of file
-                break
-            batch.append(entry)
-        if batch:
-            yield batch
 
 ### MAIN ###
 def main(rawArgs, **keywordParams):
