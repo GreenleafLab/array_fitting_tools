55163f164e32abc5dae1c8c6be2dc29b26b56546
sjkdenny <sjkdenny@gmail.com>
2018-03-23 11:35:01 -0700

    common argparse, plotting. not plt., and logging instead of print

--- bin/findFmaxDist.py
+++ bin/findFmaxDist.py
@@ -22,7 +22,7 @@ import numpy as np
 import scipy.stats as st
 from lmfit import Parameters, minimize
 import pandas as pd
-import matplotlib.pyplot as plt
+import logging
 import seaborn as sns
 import pickle
 
@@ -31,16 +31,11 @@ from fittinglibs import (fitting, plotting, fileio, processing, distribution, in
 
 
 parser = argparse.ArgumentParser(description='bootstrap fits')
-parser.add_argument('-cf', '--single_cluster_fits', required=True, metavar=".CPfitted.pkl",
-                   help='file with single cluster fits')
-parser.add_argument('-a', '--annotated_clusters', required=True, metavar=".CPannot.pkl",
-                   help='file with clusters annotated by variant number')
+processing.add_common_args(parser.add_argument_group('common arguments'),
+                           required_f=True, required_a=True, required_x=True)
 
 group = parser.add_argument_group('additional option arguments')
-group.add_argument('-out', '--out_file', 
-                   help='output filename. default is basename of input filename')
-group.add_argument('-c', '--concentrations', metavar="concentrations.txt",
-                    help='text file giving the associated concentrations')
+
 group.add_argument('-k', '--kd_cutoff', type=float,  
                    help='highest kd for tight binders (nM). default is 0.99 bound at '
                    'highest concentration')
@@ -67,6 +62,8 @@ def useSimulatedOrActual(variant_table, cutoff):
 
 if __name__=="__main__":
     args = parser.parse_args()
+    processing.update_logger(logging, args.log)
+
     fittedBindingFilename = args.single_cluster_fits
     annotatedClusterFile  = args.annotated_clusters
 
@@ -76,8 +73,7 @@ if __name__=="__main__":
     
     # find out file
     if outFile is None:
-        outFile = os.path.splitext(
-            fittedBindingFilename[:fittedBindingFilename.find('.pkl')])[0]
+        outFile = fileio.stripExtension(fittedBindingFilename) + '_init.CPvariant.gz'
         
     # make fig directory
     figDirectory = os.path.join(os.path.dirname(outFile), fileio.returnFigDirectory())
@@ -85,10 +81,10 @@ if __name__=="__main__":
         os.mkdir(figDirectory)
 
     # need to define concentrations or kd_cutoff in order to find affinity cutoff
-    if args.concentrations is not None:
-        concentrations = np.loadtxt(args.concentrations)
+    if args.xvalues is not None:
+        concentrations = np.loadtxt(args.xvalues)
     elif kd_cutoff is None:
-        print 'Error: need to either give concentrations or kd_cutoff.'
+        logging.error('Error: need to either give concentrations or kd_cutoff.')
         sys.exit()
         
     # define cutoffs
@@ -100,13 +96,13 @@ if __name__=="__main__":
         min_frac_bound = 0.95
         affinity_cutoff = parameters.find_dG_from_frac_bound(min_frac_bound, concentrations.max())
     pvalue_cutoff = args.pvalue_cutoff
-    print 'Using variants with kd less than %4.2f nM'%parameters.find_Kd_from_dG(affinity_cutoff)
-    print 'Using variants with pvalue less than %.1e'%pvalue_cutoff
+    logging.info('Using variants with kd less than %4.2f nM'%parameters.find_Kd_from_dG(affinity_cutoff))
+    logging.info('Using variants with pvalue less than %.1e'%pvalue_cutoff)
 
     # load initial fits per cluster
-    print 'Loading data...'
-    initial_points = pd.concat([pd.read_pickle(annotatedClusterFile),
-                                pd.read_pickle(fittedBindingFilename).astype(float)], axis=1)
+    logging.info('Loading data...')
+    initial_points = pd.concat([fileio.loadFile(annotatedClusterFile),
+                                fileio.loadFile(fittedBindingFilename).astype(float)], axis=1)
 
     # find variant_table
     filter_function = getattr(filterfunctions, args.filterfun)
@@ -122,32 +118,32 @@ if __name__=="__main__":
     tight_binders = variant_table.loc[good_fits&(variant_table.dG<affinity_cutoff)]
 
     # save variant table
-    variant_table.to_pickle(outFile + '_init.CPvariant.pkl')   
+    variant_table.to_csv(outFile, sep='\t', compression='gzip')   
 
     # find fmax distribution
-    print ('%d out of %d variants pass cutoff'
+    logging.info(('%d out of %d variants pass cutoff')
            %(len(tight_binders), len(variant_table)))
     if len(tight_binders) < 10:
         
-        print 'Error: need more variants passing cutoffs to fit'
-        print "Only saved init file... "
-        print sys.exit()
+        logging.error('Error: need more variants passing cutoffs to fit')
+        logging.error("Only saved init file... ")
+        sys.exit()
 
     # find good variants
     plotting.plotFmaxVsKd(variant_table.loc[good_fits], parameters.find_Kd_from_dG(affinity_cutoff))
-    plt.savefig(os.path.join(figDirectory, 'fmax_vs_kd_init.pdf'))
+    plotting.savefig(os.path.join(figDirectory, 'fmax_vs_kd_init.pdf'))
     
     plotting.plotFmaxVsKd(variant_table.loc[good_fits], parameters.find_Kd_from_dG(affinity_cutoff),
                          plot_fmin=True)
-    plt.savefig(os.path.join(figDirectory, 'fmin_vs_kd_init.pdf'))  
+    plotting.savefig(os.path.join(figDirectory, 'fmin_vs_kd_init.pdf'))  
 
     # if use_simulated is not given, decide
     if use_simulated is None:
         use_simulated = not useSimulatedOrActual(variant_table.loc[good_fits], affinity_cutoff)
     if use_simulated:
-        print 'Using fmaxes drawn randomly from clusters'
+        logging.info('Using fmaxes drawn randomly from clusters')
     else:
-        print 'Using median fmaxes of variants'
+        logging.info('Using median fmaxes of variants')
 
  
 
@@ -156,19 +152,19 @@ if __name__=="__main__":
                                 use_simulated=use_simulated,
                                 table=initial_points)
     if fmaxDist is None:
-        print "could not make fmax dist file. Only saved init file... "
+        logging.info("could not make fmax dist file. Only saved init file... ")
         sys.exit()
 
-    plt.savefig(os.path.join(figDirectory, 'counts_vs_n_tight_binders.pdf'))
-    plt.close()
-    plt.savefig(os.path.join(figDirectory, 'offset_fmax_vs_n.pdf'))
-    plt.close()
-    plt.savefig(os.path.join(figDirectory, 'stde_fmax_vs_n.pdf'))
-    plt.close()
-    plt.savefig(os.path.join(figDirectory, 'fmax_dist.all.pdf'));
+    plotting.savefig(os.path.join(figDirectory, 'counts_vs_n_tight_binders.pdf'))
+    plotting.plt.close()
+    plotting.savefig(os.path.join(figDirectory, 'offset_fmax_vs_n.pdf'))
+    plotting.plt.close()
+    plotting.savefig(os.path.join(figDirectory, 'stde_fmax_vs_n.pdf'))
+    plotting.plt.close()
+    plotting.savefig(os.path.join(figDirectory, 'fmax_dist.all.pdf'));
     
     # save
-    pickle.dump(fmaxDist, open( outFile+'.fmaxdist.p', "wb" ))
+    pickle.dump(fmaxDist, open( fileio.stripExtension(outFile)+'.fmaxdist.p', "wb" ))
         
 
     
@@ -184,7 +180,7 @@ if __name__=="__main__":
             break
         
     if len(validNs) == 0:
-        print 'Error: no number of measurements has at least %s variants. Not generating example distribution plots.'%minNumDist
+        logging.info('Error: no number of measurements has at least %s variants. Not generating example distribution plots.'%minNumDist)
     else:
         if len(validNs) <= 5:
             plotTheseNs = validNs
@@ -193,14 +189,14 @@ if __name__=="__main__":
             
         for n in plotTheseNs:
             plotting.plotAnyN(tight_binders, fmaxDist, n, bounds)
-            plt.xlim(bounds)
-            plt.savefig(os.path.join(figDirectory, 'fmax_dist.n_%d.pdf'%n))
+            plotting.plt.xlim(bounds)
+            plotting.savefig(os.path.join(figDirectory, 'fmax_dist.n_%d.pdf'%n))
 
     # plot fraction fit
     plotting.plotFractionFit(variant_table, pvalue_threshold=pvalue_cutoff)
-    plt.savefig(os.path.join(figDirectory, 'fraction_passing_cutoff_in_affinity_bins.pdf'))
-    plt.close()
-    plt.savefig(os.path.join(figDirectory, 'histogram_fraction_fit.pdf'))
+    plotting.savefig(os.path.join(figDirectory, 'fraction_passing_cutoff_in_affinity_bins.pdf'))
+    plotting.plt.close()
+    plotting.savefig(os.path.join(figDirectory, 'histogram_fraction_fit.pdf'))
     
 
     
